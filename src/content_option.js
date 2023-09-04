bdf84d0bf0f2a73d35bb3b8cae8a2684bdec8904
Ibrhaim Abdullah <ibrhaimimtiaz59@gmail.com>
2023-09-04 19:38:25 +0500

    firstCommit

---
+++ src/content_option.js
@@ -0,0 +1,184 @@
+import aradchain from "./assets/images/aradchain.jpeg";
+import rwax from "./assets/images/rwax.jpeg";
+import jortty from "./assets/images/jortty.jpeg";
+import mettaprotocol from "./assets/images/mettaprotocol.jpeg";
+import terramater from "./assets/images/terramater.jpeg";
+
+const logotext = "Ibrahim";
+const meta = {
+  title: "Ibrahim Abdullah",
+  description:
+    "I’m Ibrahim Abdullah Web Developer _ Full stack devloper,currently working as freelancer",
+};
+
+const introdata = {
+  title: "I’m Ibrahim Abdullah",
+  animated: {
+    first: "I have a genuine passion for coding websites",
+    second: "I thoroughly enjoy the art of coding.",
+    third: "I specialize in creating responsive web applications.",
+  },
+  description:
+    "A passionate and results-driven web app developer with a rock-solid track record of 2 years in crafting digital solutions that not only meet but exceed client expectations",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "With 2 years of hands-on experience, I've honed my skills in a wide array of web development technologies, including HTML5, CSS3, JavaScript, React.js, Node.js, and more. I have a deep understanding of both frontend and backend development, allowing me to create seamless, end-to-end web applications.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Web application developer",
+    where: "Freelancer",
+    date: "current",
+  },
+  {
+    jobtitle: "Web Developer",
+    where: "Brdigitech",
+    date: "2022-2023",
+  },
+  {
+    jobtitle: "Telesales Agent",
+    where: "Atrium Solutions",
+    date: "2020-2021",
+  },
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React JS",
+    value: 90,
+  },
+  {
+    name: "Next JS",
+    value: 85,
+  },
+
+  {
+    name: "Node JS",
+    value: 70,
+  },
+  {
+    name: "Mongo DB",
+    value: 70,
+  },
+  {
+    name: "Express JS",
+    value: 70,
+  },
+  {
+    name: "HTML",
+    value: 90,
+  },
+  {
+    name: "CSS",
+    value: 90,
+  },
+  {
+    name: "Tailwind CSS",
+    value: 80,
+  },
+  {
+    name: "Chakra UI",
+    value: 80,
+  },
+  {
+    name: "Strapi",
+    value: 80,
+  },
+  {
+    name: "Contentful",
+    value: 80,
+  },
+];
+
+const services = [
+  {
+    title: "Front-end Development",
+    description:
+      "Create stunning user interfaces and engaging user experiences with cutting-edge front-end technologies like HTML5, CSS3, and JavaScript. We specialize in crafting pixel-perfect designs that captivate your audience and keep them coming back for more.",
+  },
+  {
+    title: "Back-end Development",
+    description:
+      "Build robust and scalable back-end systems that power your web applications. Our expertise in server-side technologies ensures seamless data handling, secure user authentication, and efficient database management to keep your application running smoothly.",
+  },
+  {
+    title: "Figma to React",
+    description:
+      "Transform your Figma designs into interactive React components. Our team of experienced developers can turn your static designs into dynamic, responsive web interfaces that perfectly match your vision, all while maintaining design fidelity.",
+  },
+  {
+    title: "Responsive Design",
+    description:
+      " Ensure your website looks and functions flawlessly on all devices, from desktops to smartphones. Our responsive design approach guarantees that your content adapts seamlessly to different screen sizes, delivering an optimal user experience every time.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: aradchain,
+    description:
+      "Landing page for a decentralised finance platform. The goal was to create a simple and clean design.",
+    link: "https://aradchain-tech.netlify.app/",
+  },
+  {
+    img: rwax,
+    description:
+      "A claiming DApp and a landing page. Effortless claiming of tokens for users.",
+    link: "http://rwax-ico.surge.sh/",
+  },
+  {
+    img: jortty,
+    description:
+      "Landing Page for an AI plateform. Responsive design with a clean and modern look.",
+    link: "http://jorrty-nextjs-static.s3-website-us-east-1.amazonaws.com/",
+  },
+  {
+    img: terramater,
+    description:
+      "NFT marketplace for digital art. Swapping native tokens for terramater echo-system",
+    link: "https://www.terramaternfts.com/swapping",
+  },
+  {
+    img: mettaprotocol,
+    description:
+      "A complete decentralized application with all the features etc. Swapping, Staking, Minting, Farming, etc.",
+    link: "https://mettaprotocol.io/",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "ibrahimimtiaz59@gmail.com",
+  YOUR_FONE: "+92 318 0630775",
+  description:
+    "Feel free to contact me for any work or suggestions. I will try my best to get back to you ASAP. My inbox is always open for any queries.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_uf8agkr",
+  YOUR_TEMPLATE_ID: "template_8w6mimb",
+  YOUR_USER_ID: "Yhtn-woyGqkkomcJF",
+};
+
+const socialprofils = {
+  github: "https://github.com/PragramAllDay",
+  linkedin: "https://www.linkedin.com/in/ibrahim-abdullah-498362247/",
+  twitter: "https://twitter.com/Ibrahim40251006",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
